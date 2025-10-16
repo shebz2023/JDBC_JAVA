fbd4b6c84eaebbe9c249bb4e662fa1e85b79b374
shebz2023 <shebelleke@gmail.com>
2025-10-16 13:35:35 +0200

    [Add]search

--- SearchEvent.java
+++ SearchEvent.java
@@ -3,7 +3,7 @@ import java.sql.*;
 import java.util.Properties;
 import java.util.Scanner;
 
-class SearchEVent {
+class SearchEvent {
     public static void search(String title) {
         try {
             Properties props = new Properties();
@@ -12,20 +12,22 @@ class SearchEVent {
             String url = props.getProperty("db.url");
             String name = props.getProperty("db.name");
             String password = props.getProperty("db.password");
-            String query = "select title from events";
-            Connection con = DriverManager.getConnection(url, name, password );
+            String query = "select * from events Where title = ?";
+            Connection con = DriverManager.getConnection(url, name, password);
             System.out.println("connection instablished successfully");
-            Statement stmt = con.createStatement();
-            ResultSet rs = stmt.executeQuery(query);
-            System.out.println(rs);
-            while (rs.next()) {
-                String a = rs.getString("title");
-                System.out.println(a + "==== res =====");
+            PreparedStatement stmt = con.prepareStatement(query);
+            stmt.setString(1, title);
+            ResultSet rs = stmt.executeQuery();
+            if (rs.next()) {
+                String tit = rs.getString("title");
+                String desc = rs.getString("description");
+                System.out.println(tit + "\n" + "Description:" + desc);
+            } else {
+                System.out.println("Not found");
             }
             stmt.close();
             con.close();
         } catch (Exception e) {
-            // TODO: handle exception
             System.out.println("====== The error =====>>> " + e);
         }
     }
